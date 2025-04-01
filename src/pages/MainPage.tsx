3eb4ab3d8ba323ca871dd91bcacd39dfd4a3b2f8
alan.batchaev <alanchik99@gmail.com>
2025-04-01 19:14:33 +0300

    test

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -10,19 +10,20 @@ export const MainPage = () => {
   const [state, setState] = useState<TState>('default')
   const [generatedNumbers, setGeneratedNumbers] = useState<number[]>([])
   const [errorText, setErrorText] = useState('')
-  const [rolling, setRolling] = useState(false)
-  const [currentNumber, setCurrentNumber] = useState<number | null>(null)
+  const [animate, setAnimate] = useState(false)
 
   const { inputState, handleChange } = useInputState()
+
   const wrapperRef = useRef<HTMLDivElement>(null)
+  const sexRef = useRef<HTMLDivElement>(null)
   const numberRef = useRef<HTMLDivElement | null>(null)
 
   const handleGenerateNumber = () => {
-    if (rolling) return // Блокируем повторное нажатие
-
     const { from, to, repeat } = inputState
 
     try {
+      setAnimate(false)
+
       const fromNum = Number(from)
       const toNum = Number(to)
 
@@ -34,22 +35,13 @@ export const MainPage = () => {
         throw new Error('Минимальное значение должно быть меньше максимального')
       }
 
-      setRolling(true) // Начинаем вращение
-      let tempNumber = fromNum
-
-      const interval = setInterval(() => {
-        tempNumber = Math.floor(Math.random() * (toNum - fromNum + 1)) + fromNum
-        setCurrentNumber(tempNumber) // Постоянно меняем число
-      }, 50) // Скорость вращения
+      const newNumber = generateNumber(fromNum, toNum, repeat, generatedNumbers)
 
       setTimeout(() => {
-        clearInterval(interval)
-        const finalNumber = generateNumber(fromNum, toNum, repeat, generatedNumbers)
-        setGeneratedNumbers([finalNumber, ...generatedNumbers])
-        setCurrentNumber(finalNumber)
-        setRolling(false) // Останавливаем рулетку
-      }, 2000) // 2 секунды вращения
+        setAnimate(true)
+      }, 100)
 
+      setGeneratedNumbers((prev) => [newNumber, ...prev])
       setErrorText('')
     } catch (error: any) {
       setErrorText(error.message)
@@ -60,7 +52,6 @@ export const MainPage = () => {
     setGeneratedNumbers([])
     setErrorText('')
     setState('start')
-    setCurrentNumber(null)
   }
 
   useEffect(() => {
@@ -81,21 +72,27 @@ export const MainPage = () => {
   }
 
   return (
-    <div ref={wrapperRef} className={styles.wrapper}>
+    <div ref={wrapperRef} className={`${styles.wrapper}`}>
       <div className={styles.container}>
         <Axenix />
         <div className={styles.textContainer}>
           {!generatedNumbers.length && <h2 className={styles.title}>{getTitle()}</h2>}
-          <div ref={numberRef} className={`${styles.numberContainer} ${rolling ? styles.spinning : ''}`}>
-            <span className={styles.number}>{currentNumber ?? '—'}</span>
-          </div>
           {!!generatedNumbers.length && (
-            <div className={`${styles.numberContainer} ${styles.oldNumbers}`}>
-              {generatedNumbers.slice(1, 6).map((num, index) => (
-                <span key={index} className={`${styles.number} ${styles.small}`}>
-                  {num}
-                </span>
-              ))}
+            <div ref={numberRef} className={styles.numberContainer}>
+              <span className={`${styles.number} ${styles.main} ${animate ? styles.numberSlide : ''}`}>
+                {generatedNumbers[0]}
+              </span>
+            </div>
+          )}
+          {!!generatedNumbers.length && (
+            <div ref={sexRef} className={`${styles.numberContainer} ${styles.oldNumbers}`}>
+              {generatedNumbers.slice(1, 6).map((num, index) => {
+                return (
+                  <span key={index} className={`${styles.number} ${styles.small}`}>
+                    {num}
+                  </span>
+                )
+              })}
             </div>
           )}
           {state === 'default' && <p className={styles.description}>Генератор случайных чисел</p>}
