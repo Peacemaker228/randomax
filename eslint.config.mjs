dcbbceb25fa37c8d3fa5dc7300d4fa6fb323c622
alan.batchaev <alanchik99@gmail.com>
2025-04-03 20:23:57 +0300

    deploy fix 10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,18 +3,22 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import typescript from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'], // Добавил ts и tsx
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsParser, // Указываем TypeScript-парсер
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
+        project: './tsconfig.json', // Указываем путь к tsconfig.json
       },
     },
     settings: { react: { version: '18.3' } },
@@ -22,17 +26,16 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@typescript-eslint': typescript, // Добавляем плагин TypeScript
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      ...typescript.configs.recommended.rules, // Добавляем TS-правила
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
 ]
